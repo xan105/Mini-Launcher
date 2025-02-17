a2ccaa5056375d7fc32dad28181cb374ca22abd8
Anthony Beaumont <dev.xan105@outlook.be>
2025-02-17 10:13:39 +0700

    add --dry-run flag

--- src/cmdLine.go
+++ src/cmdLine.go
@@ -13,19 +13,23 @@ import(
 
 type Args struct {
   Config     string
+  DryRun     bool
   Help       bool
 }
 
 func parseArgs() (args Args) {
   flag.StringVar(&args.Config, "config", "launcher.json", "File path to the config file to use.")
+  flag.BoolVar(&args.DryRun, "dry-run", false, "Program will exit before starting the executable.")
   flag.BoolVar(&args.Help, "help", false, "Show list of all arguments")
   flag.Parse()
   if args.Help { 
     alert( 
-      "-config filePath\n" +
+      "--config filePath\n" +
       "File path to the config file to use.\n" +
+      "--dry-run\n" +
+      "Program will exit before starting the executable.\n" +
       "\n" +
-      "-help\n" +
+      "--help\n" +
       "Show list of all arguments\n",
     )
     os.Exit(0)
