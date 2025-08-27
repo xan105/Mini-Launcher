dec67207cffec9e59a32ab5faa4b3f897302f7a9
Anthony Beaumont <dev.xan105@outlook.be>
2025-08-27 11:33:48 +0700

    Update CDX-RUNE.lua

--- example/lua script/steam emu/CDX-RUNE.lua
+++ example/lua script/steam emu/CDX-RUNE.lua
@@ -1,20 +1,30 @@
-local file = require("file")
 local user = require("user")
+local file = require("file")
 local INI = require("config/ini")
 
 local filePath = "steam_emu.ini"
 
 local cfg, err = INI.Parse(file.Read(filePath))
 if not err then
-  if cfg["Settings"] and cfg["Settings"]["UserName"] == "" then
+  local update = false
+  if cfg["Settings"] then
+    if cfg["Settings"]["UserName"] == "" or 
+       cfg["Settings"]["UserName"] == "CODEX" or 
+       cfg["Settings"]["UserName"] == "RUNE" then
      cfg["Settings"]["UserName"] = user.name
+     update = true
+     end
   end
+  
   if cfg["Settings"] and cfg["Settings"]["Language"] == "" then
      cfg["Settings"]["Language"] = user.language
+     update = true
   end
-
-  file.Write(filePath, INI.Stringify(cfg, { 
-    whitespace = false, 
-    blankLine = true 
-  }))
+  
+  if update then
+    file.Write(filePath, INI.Stringify(cfg, { 
+      whitespace = false, 
+      blankLine = true 
+    }))
+  end
 end
